c9a8d6a8353143f406cb681ec6868e89a8717428
Carlos Londono <c.londono.m@gmail.com>
2022-08-02 21:31:40 -0500

    styling Header comp

--- components/Header.js
+++ components/Header.js
@@ -5,20 +5,22 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className='container'>
-        <Link href='/'>
-          <Image
-            src='/img/logo.svg'
-            width={400}
-            height={100}
-            alt='image logo'
-          />
-        </Link>
-        <nav className='navigation'>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About Us</Link>
-          <Link href='/blog'>Blog</Link>
-          <Link href='/store'>Store</Link>
-        </nav>
+        <div className={styles.bar}>
+          <Link href='/'>
+            <Image
+              src='/img/logo.svg'
+              width={400}
+              height={100}
+              alt='image logo'
+            />
+          </Link>
+          <nav className={styles.navigation}>
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About Us</Link>
+            <Link href='/blog'>Blog</Link>
+            <Link href='/store'>Store</Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
