d42296a09a035cec2dc1a7cf32f234de5d519c4f
Carlos Londono <c.londono.m@gmail.com>
2022-08-02 19:55:51 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <Link href='/'>Home</Link>
+    </div>
+  );
+};
+
+export default AboutUs;
